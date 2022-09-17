dcfe9d8dd0abf355232f2b0e2cb35efa7d563b23
Suranjan <suranjanbiswas2002@gmail.com>
2022-09-17 16:18:58 +0530

    add: added utils functions for emails

--- controllers/users.controllers.ts
+++ controllers/users.controllers.ts
@@ -1,9 +1,20 @@
 import { Request, Response } from 'express'
+import UserModel from '../models/users.models'
+import { validateEmail } from '../utils/functions'
 
 const test = (_req: Request, res: Response) => {
   res.status(200).json({
     message: 'User-controller works!'
   })
+}
+
+const signup = async (req: Request, res: Response) => {
+  const { name, email } = req.body
+  if (!name || !email || !validateEmail(email)) {
+    return res.status(400).json({
+      message: 'Either name or email is missing or email is invalid!'
+    })
+  }
 }
 
 export { test }
