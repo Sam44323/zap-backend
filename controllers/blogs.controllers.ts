d2a26626eec1cabcdbe0aa4aa1591b5ee6f20c2b
Suranjan <suranjanbiswas2002@gmail.com>
2022-09-17 17:21:28 +0530

    aadd: added code for creating a new blog with the tokens

--- controllers/blogs.controllers.ts
+++ controllers/blogs.controllers.ts
@@ -10,7 +10,33 @@ const test = (_req: Request, res: Response) => {
 
 const getBlogs = async (req: Request, res: Response) => {}
 
-const createBlog = async (req: Request, res: Response) => {}
+const createBlog = async (req: Request, res: Response) => {
+  const { title, description } = req.body
+  if (!title || !description || !req.headers.authorization) {
+    return res.status(400).json({
+      message: 'Bad request'
+    })
+  }
+  const decipheredToken = decipherToken(req.headers.authorization)
+  const author = decipheredToken.email
+  try {
+    const blog = new BlogsModel({
+      title,
+      description,
+      author,
+      createdAt: new Date().toISOString()
+    })
+    await blog.save()
+    return res.status(201).json({
+      message: 'Blog created successfully'
+    })
+  } catch (err) {
+    console.error(err)
+    return res.status(500).json({
+      message: 'Internal server error'
+    })
+  }
+}
 
 const updateBlog = async (req: Request, res: Response) => {}
 
