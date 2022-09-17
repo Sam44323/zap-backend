b62802be9204dff43eb19b7a1701fb1def9c3b12
Suranjan <suranjanbiswas2002@gmail.com>
2022-09-17 16:53:40 +0530

    add: added code for controllers

--- routes/users.routes.ts
+++ routes/users.routes.ts
@@ -1,7 +1,15 @@
 import { Router } from 'express'
-import { test } from '../controllers/users.controllers'
+import {
+  deleteUser,
+  login,
+  signup,
+  test
+} from '../controllers/users.controllers'
 
 const userRouter = Router()
 userRouter.get('/test', test)
+userRouter.post('/signup', signup)
+userRouter.post('/login', login)
+userRouter.delete('/delete', deleteUser)
 
 export default userRouter
