b4225bcfcec0d51168553486de340a3f92e55087
Suranjan <suranjanbiswas2002@gmail.com>
2022-09-17 16:09:43 +0530

    add: added initial descriptors for the test-controllers

--- routes/blogs.routes.ts
+++ routes/blogs.routes.ts
@@ -1,5 +1,7 @@
 import { Router } from 'express'
+import { test } from '../controllers/blogs.controllers'
 
 const blogsRouter = Router()
+blogsRouter.get('/test', test)
 
 export default blogsRouter
--- routes/users.routes.ts
+++ routes/users.routes.ts
@@ -1,5 +1,7 @@
 import { Router } from 'express'
+import { test } from '../controllers/users.controllers'
 
 const userRouter = Router()
+userRouter.get('/test', test)
 
 export default userRouter
