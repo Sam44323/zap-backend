1dbf4a6f1d9109a42306a3dd5ecde79879cbd384
Suranjan <suranjanbiswas2002@gmail.com>
2022-09-17 15:52:04 +0530

    add: added the model for blogs

---
+++ models/blogs.models.ts
@@ -0,0 +1,21 @@
+import { prop, getModelForClass, Ref } from '@typegoose/typegoose'
+import mongoose from 'mongoose'
+
+class Blogs {
+  @prop({ required: true, type: String })
+  title: string
+
+  @prop({ required: true, type: String })
+  description: string
+
+  @prop({ required: true, type: mongoose.Schema.Types.ObjectId, ref: 'Users' })
+  author: Ref<mongoose.Schema.Types.ObjectId>
+}
+
+const BlogsModel = getModelForClass(Blogs, {
+  options: {
+    customName: 'Blogs'
+  }
+})
+// mongoose.model('Blogs')
+export default BlogsModel
